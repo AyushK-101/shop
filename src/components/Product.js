eab6e1b6fde0b9ee6ce5a4582bed9a5811a0a175
Ayush Kumar <ayushbhaiya100@gmail.com>
2023-08-21 17:33:41 +0530

    Update Product.js
    <div className="flex mx-auto justify-center h-[250px] w-[250px]">
           <Image src={image} height={200} width={200} alt="" />
          </div>

--- src/components/Product.js
+++ src/components/Product.js
@@ -42,11 +42,11 @@ function Product({id, title, price, description, category, image}) {
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
 
-      <div className="flex justify-center">
+      
         <div className="flex mx-auto justify-center h-[250px] w-[250px]">
            <Image src={image} height={200} width={200} alt="" />
         <div>
-      </div>
+      
 
       <h4 className="my-3">{title}</h4>
 
